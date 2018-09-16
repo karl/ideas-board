391970bad885eecf99a643604f41cc57684b4c34
Karl O'Keeffe <github@monket.net>
2018-09-16 20:42:38 +0100

    Add Prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Ideas Board</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Ideas Board</h1>
+      </div>
+    );
+  }
 }
