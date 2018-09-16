b6991b0b4e4281035c7410ba7ae3b58e2d4a55f0
Karl O'Keeffe <github@monket.net>
2018-09-16 21:03:37 +0100

    Very basic styling and layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const testIdeas = [
+  {
+    id: 1,
+    title: 'A test idea',
+    description: 'First line\nSecond line\nThird line',
+    createdDate: new Date(),
+  },
+  {
+    id: 2,
+    title: 'Editable titles',
+    description: 'Make titles editable',
+    createdDate: new Date(),
+  },
+];
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideas: testIdeas,
+    };
+  }
+
   render() {
+    const { ideas } = this.state;
     return (
       <div>
-        <h1>Ideas Board</h1>
+        <div className="header">
+          <button className="new">+ New Idea</button>
+          <h1>Ideas Board</h1>
+        </div>
+        <div>
+          {ideas.map((idea) => (
+            <div key={idea.id} className="idea">
+              <input
+                className="title"
+                type="text"
+                placeholder="title"
+                value={idea.title}
+                onChange={() => {}}
+              />
+              <textarea
+                className="description"
+                placeholder="description"
+                value={idea.description}
+                onChange={() => {}}
+              />
+              <button className="delete">Delete</button>
+              <div className="createdDate">
+                Created {idea.createdDate.toString()}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
